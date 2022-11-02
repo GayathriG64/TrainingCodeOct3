c4a83a159bba1e5bba26a566a2373fa2eb381753
unknown <cogjava3256@iiht.tech>
2022-11-02 14:15:27 +0530

    added microservice and reader entity for use case

--- UseCase_Practice/UserService/src/main/java/com/digitalbooks/controller/AuthorController.java
+++ UseCase_Practice/UserService/src/main/java/com/digitalbooks/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.digitalbooks.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,4 +26,6 @@ public class AuthorController {
 		System.out.println(book);
 		return book;
 	}
+	
+	
 }
